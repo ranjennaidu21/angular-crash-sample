c907046881187b57090e768da43c81324be6db66
ranjennaidu21 <ranjennaidu21@gmail.com>
2018-12-20 12:10:40 +0800

    directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -11,10 +11,24 @@ import {Component} from '@angular/core';
     selector: 'courses', // <courses>
 
     //data-binding using {{}} will render dynamically: when title value changes , the view automatically notified and changes
-    template: '<h2>{{ "Title: " + title + " Using method: " + getTitle()}}</h2>' //html markup 
+    //now we use `` as we can put element inside this template in multiple lines
+    template: `
+    <h2>{{"Title: " + title}}</h2>
+    <h2>{{"Using method: " + getTitle()}}</h2>
+    <ul>
+        <!--use ngFor directives to manipulate the DOM, use it like html attribute
+        use * in front of ng whenver modifying the structure of DOM by adding or removing the element.
+        we defining course variable for each of the value in courses array below
+        -->
+        <li *ngFor="let course of courses">
+        {{ course }}
+        </li>
+    </ul>
+    `
 })
 export class CoursesComponent{
     title = "List of courses";
+    courses = ["course1","course2","course3"];
 
     getTitle(){
         return this.title;
